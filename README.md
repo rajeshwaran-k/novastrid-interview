5062eb5cbe03744a45b222aeec5c39614fe52385
Rajeshwaran <110971058+rajeshwaran-k@users.noreply.github.com>
2025-01-13 12:57:06 +0530

    fix env issue

--- README.md
+++ README.md
@@ -6,7 +6,19 @@
 
 ## Setting up
 
-- After clonning the application locally run the following command
+- After clonning the application create .env and add following values with your DB cred,
+  DB_HOST
+  DB_PORT
+  DB_USERNAME
+  DB_PASSWORD
+
+- Execute follwing command to run migartion
+
+```
+npx mikro-orm migration:up
+```
+
+- Start the application by following command
 
 ```
 npm run start:dev
@@ -14,4 +26,4 @@ npm run start:dev
 
 - Application will be running in port 3000
 - Import `postman-sample-request.json` into your Postman application. You will be able to see sample requests for login and signup.
-- Use `http://localhost:3000/auth/chat/bulkupload` to upload excel sheet. You can also find a `sample-sheet.xlsx` in this repo.
+- Use `http://localhost:3000/auth/chat/bulkupload` to upload excel sheet. You can also find a `sample-sheet.xlsx` in this repo. Use key name as `file`.
--- mikro-orm.config.ts
+++ mikro-orm.config.ts
@@ -4,7 +4,7 @@ import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
 import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { Migrator } from '@mikro-orm/migrations';
-
+require('dotenv').config();
 
 const logger = new Logger('MikroORM');
 
@@ -12,11 +12,10 @@ const config = defineConfig({
   entities: ['dist/**/*.entity.js'],
   entitiesTs: ['src/**/*.entity.ts'],
   driver: PostgreSqlDriver,
-  //todo: env values not working need to check, hardcoding for now.
-  dbName: 'test-dev',
-  host: 'stage-postgres-db-cluster.cluster-clh8m7tanhah.ap-south-1.rds.amazonaws.com',
-  password: 'eYZxX94adwMXgWk',
-  user: 'adminsl',
+  dbName: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USERNAME,
   port: parseInt(process.env.DB_PORT || '5432', 10),
   highlighter: new SqlHighlighter(),
   debug: true,
